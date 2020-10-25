6f378722ba71f4d80ffce44df0a93c9e6b992902
Victor <v.huang435@gmail.com>
2020-10-25 21:59:58 +1100

    phase 2

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -1,59 +1,140 @@
-pragma solidity 0.5.12;
-import "./Owner.sol";
+pragma solidity >=0.5.12 < 0.6.0;
+import "./Ownable.sol";
+import "./provableAPI.sol";
 
 
-contract CoinFlip is Ownable{
+
+contract CoinFlip is Ownable, usingProvable{
 
 
     uint internal balance;
     uint internal betAmount;
-    uint internal result;
+    //uint internal result;
     uint internal reward;
+
+
+    bytes32 id;
+
+
     event youWin (string, uint);
     event youLose(string, uint);
-  //  address player;
+
+    struct result{
+        uint value;
+        bool isWin;
+    }
+
+
+    struct bet{
+        address player;
+        uint betAmount;
+    }
+
+    mapping (bytes32 => bet) private waiting;
+      mapping (uint => result) private results;
+
 
 
      modifier costs(uint cost){
-        require(msg.value >= cost, "Need at least 100 Wei");
+        require(msg.value >= cost, "Need at least 0.1 szabo");
         _;
 
 
     }
 
+    uint256 constant NUM_RANDOM_BYTES_REQUESTED = 1;
+  uint256 public latestNumber;
 
-    function play()public payable costs(1 ether){
+
+  event LogNewProvableQuery(string description);
+  event generatedRandomNumber(uint256 randomNumber);
+  event idCheck(string ss, bytes32 id);
+
+
+  //constructor() public {
+  //update();
+  //}
+
+  function __callback(bytes32 _queryId, string memory _result, bytes memory _proof) public {
+    require(msg.sender == provable_cbAddress());
+
+    uint256 randomNumber = uint256(keccak256(abi.encodePacked(_result))) % 100;
+
+    latestNumber = randomNumber;
+    saveResults(latestNumber);
+    winOrLose();
+
+    delete waiting[id];
+
+
+
+    emit generatedRandomNumber(randomNumber);
+  }
+
+  function random() payable public costs (0.1 szabo) {
+    uint256 QUERY_EXECUTION_DELAY =0;
+    uint GAS_FOR_CALLBACK = 200000;
+     id =
+    provable_newRandomDSQuery(
+        QUERY_EXECUTION_DELAY,
+        NUM_RANDOM_BYTES_REQUESTED,
+        GAS_FOR_CALLBACK
+      );
+
+     updatePlayer(msg.value);
+     emit idCheck("The ID of this call is ",id);
+    emit LogNewProvableQuery("Provable query was sent, standing by for the answer.");
+
+  }
+
+    function saveResults(uint value) public{
+        result memory newResult;
+        newResult.value = latestNumber;
+
+        if (latestNumber % 2 ==1){
+            newResult.isWin = true;
+        }
+
+        else {
+            newResult.isWin = false;
+        }
+
+        results[latestNumber] = newResult;
+    }
+
+
+
+    function updatePlayer(uint _betAmount )public payable  {
+        //update();
+
+        bet memory newPlayer;
+        newPlayer.player = msg.sender;
+        newPlayer.betAmount = _betAmount;
+        waiting[id] = newPlayer;
+
+
         betAmount += msg.value;
-       // reward += msg.value;
-
-
-        random();
-        winOrLose();
 
 
 
-    }
 
-    function valueInput()public view returns(uint){
+       // winOrLose();
 
-        return betAmount;
+
 
     }
 
-  /*  function initialBalance() public payable{
-      balance = msg.value;
-     */
 
-    //random function()
-    function random() internal returns(uint){
 
-            result = now % 2;
-        return result;
+
+    function getResults(bytes32 id) internal returns (address player, uint bets){
+         return (waiting[id].player, waiting[id].betAmount);
+
 
     }
 
     function winOrLose() internal returns (uint){
-        if (result == 1){
+        if (results[latestNumber].isWin == true){
             //pay player
             uint toReward = betAmount*2;
 
@@ -88,6 +169,7 @@ contract CoinFlip is Ownable{
       return balance;
 
     }
+
 
 
 
--- migrations/2_coinflip_migration.js
+++ migrations/2_coinflip_migration.js
@@ -2,7 +2,7 @@ const CoinFlip = artifacts.require("CoinFlip");
 
 module.exports = function(deployer) {
   deployer.deploy(CoinFlip).then(function(instance){
-    instance.depositFunds({value: web3.utils.toWei("50", "ether")})
+    instance.depositFunds({value: web3.utils.toWei("0.1", "szabo")})
 
   })
 
