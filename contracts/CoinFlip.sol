3a989ea540f1f52e39e27f0e70b5fb15f106e088
Victor <v.huang435@gmail.com>
2020-10-26 23:24:25 +1100

    changed structs

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -7,168 +7,177 @@ import "./provableAPI.sol";
 contract CoinFlip is Ownable, usingProvable{
 
 
-    uint internal balance;
-    uint internal betAmount;
-    //uint internal result;
-    uint internal reward;
+  uint internal balance;
+  uint internal betAmount;
+  //uint internal result;
+  //uint internal reward;
 
 
-    bytes32 id;
+  bytes32 id;
 
 
-    event youWin (string, uint);
-    event youLose(string, uint);
+  event youWin (string, uint);
+  event youLose(string, uint);
 
-    struct result{
-        uint value;
-        bool isWin;
-    }
-
-
-    struct bet{
-        address player;
-        uint betAmount;
-    }
-
-    mapping (bytes32 => bet) private waiting;
-      mapping (uint => result) private results;
-
-
-
-     modifier costs(uint cost){
-        require(msg.value >= cost, "Need at least 0.1 szabo");
-        _;
-
-
-    }
-
-    uint256 constant NUM_RANDOM_BYTES_REQUESTED = 1;
-  uint256 public latestNumber;
-
-
-  event LogNewProvableQuery(string description);
-  event generatedRandomNumber(uint256 randomNumber);
-  event idCheck(string ss, bytes32 id);
-
-
-  //constructor() public {
-  //update();
-  //}
-
-  function __callback(bytes32 _queryId, string memory _result, bytes memory _proof) public {
-    require(msg.sender == provable_cbAddress());
-
-    uint256 randomNumber = uint256(keccak256(abi.encodePacked(_result))) % 100;
-
-    latestNumber = randomNumber;
-    saveResults(latestNumber);
-    winOrLose();
-
-    delete waiting[id];
-
-
-
-    emit generatedRandomNumber(randomNumber);
+  struct result{
+      uint value;
+      bool isWin;
+      bytes32 id;
   }
 
-  function random() payable public costs (0.1 szabo) {
-    uint256 QUERY_EXECUTION_DELAY =0;
-    uint GAS_FOR_CALLBACK = 200000;
-     id =
-    provable_newRandomDSQuery(
-        QUERY_EXECUTION_DELAY,
-        NUM_RANDOM_BYTES_REQUESTED,
-        GAS_FOR_CALLBACK
-      );
 
-     updatePlayer(msg.value);
-     emit idCheck("The ID of this call is ",id);
-    emit LogNewProvableQuery("Provable query was sent, standing by for the answer.");
+  struct bet{
+      address player;
+      uint betAmount;
+      bytes32 id;
+  }
+
+
+
+  mapping (bytes32 => bet) private waiting;
+    mapping (uint => result) private results;
+
+
+
+   modifier costs(uint cost){
+      require(msg.value >= cost, "Need at least 0.1 szabo");
+      _;
+
 
   }
 
-    function saveResults(uint value) public{
-        result memory newResult;
-        newResult.value = latestNumber;
-
-        if (latestNumber % 2 ==1){
-            newResult.isWin = true;
-        }
-
-        else {
-            newResult.isWin = false;
-        }
-
-        results[latestNumber] = newResult;
-    }
+  uint256 constant NUM_RANDOM_BYTES_REQUESTED = 1;
+uint256 public latestNumber;
 
 
-
-    function updatePlayer(uint _betAmount )public payable  {
-        //update();
-
-        bet memory newPlayer;
-        newPlayer.player = msg.sender;
-        newPlayer.betAmount = _betAmount;
-        waiting[id] = newPlayer;
+event LogNewProvableQuery(string description);
+event generatedRandomNumber(uint256 randomNumber);
+event idCheck(string ss, bytes32 id);
 
 
-        betAmount += msg.value;
+//constructor() public {
+//update();
+//}
+
+function __callback(bytes32 _queryId, string memory _result, bytes memory _proof) public {
+  require(msg.sender == provable_cbAddress());
+
+  uint256 randomNumber = uint256(keccak256(abi.encodePacked(_result))) % 2;
+
+  latestNumber = randomNumber;
+  saveResults(_queryId,latestNumber);
+
+  winOrLose();
+
+  delete waiting[id];
 
 
 
 
-       // winOrLose();
+  emit generatedRandomNumber(randomNumber);
+}
+
+function random() payable public costs (10000 wei) {
+  uint256 QUERY_EXECUTION_DELAY =0;
+  uint GAS_FOR_CALLBACK = 200000;
+   id =
+  provable_newRandomDSQuery(
+      QUERY_EXECUTION_DELAY,
+      NUM_RANDOM_BYTES_REQUESTED,
+      GAS_FOR_CALLBACK
+    );
+
+   updatePlayer(msg.value);
+   emit idCheck("The ID of this call is ",id);
+  emit LogNewProvableQuery("Provable query was sent, standing by for the answer.");
+
+}
+
+  function saveResults(bytes32, uint value) public{
+      result memory newResult;
+      newResult.value = latestNumber;
+      newResult.id = id;
+
+
+      if (latestNumber  ==1){
+          newResult.isWin = true;
+      }
+
+      else {
+          newResult.isWin = false;
+      }
+
+      results[latestNumber] = newResult;
+  }
 
 
 
-    }
+  function updatePlayer(uint _betAmount )public payable  {
+      //update();
+
+      bet memory newPlayer;
+      newPlayer.player = msg.sender;
+      newPlayer.betAmount = _betAmount;
+      waiting[id] = newPlayer;
+
+
+      betAmount += msg.value;
 
 
 
 
-    function getResults(bytes32 id) internal returns (address player, uint bets){
-         return (waiting[id].player, waiting[id].betAmount);
+     // winOrLose();
 
 
-    }
 
-    function winOrLose() internal returns (uint){
-        if (results[latestNumber].isWin == true){
-            //pay player
-            uint toReward = betAmount*2;
+  }
 
-            msg.sender.transfer(toReward);
-            balance -= betAmount;
 
-            emit youWin("You won ", toReward);
-            toReward  =0;
-        }
 
-        else {
-            //keep money
-            balance += betAmount;
-            emit youLose("You lost ", betAmount);
-        }
 
-        reward = betAmount;
-        betAmount =0;
-    }
+  function getResults(bytes32 id) internal returns (address player, uint bets){
+       return (waiting[id].player, waiting[id].betAmount);
 
-    function getBalance() public view returns(uint){
-        return balance;
 
-    }
+  }
 
-    function getReward() public view returns(uint){
-        return reward;
-    }
+  function winOrLose() internal returns (uint){
+      if (results[latestNumber].isWin == true){
+          //pay player
 
-    function depositFunds() public onlyOwner payable returns(uint){
-      balance += msg.value;
+          uint toReward = waiting[id].betAmount * 2;
+
+          msg.sender.transfer(toReward);
+          balance -= waiting[id].betAmount;
+
+          emit youWin("You won ", toReward);
+          toReward  =0;
+      }
+
+      else {
+          //keep money
+          balance += waiting[id].betAmount;
+          emit youLose("You lost ", waiting[id].betAmount);
+      }
+
+
+      waiting[id].betAmount = 0;
+  }
+
+  function getBalance() public view returns(uint){
       return balance;
 
-    }
+  }
+
+ // function getReward() public view returns(uint){
+ //     return reward;
+ // }
+
+  function depositFunds() public onlyOwner payable returns(uint){
+    balance += msg.value;
+    return balance;
+
+  }
 
 
 
